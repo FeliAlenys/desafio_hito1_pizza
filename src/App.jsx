d0c66f0bef2400c7754ee4e4ef7e8f986057ee28
Felipe Gonzalez <felipe.gonzatr@gmail.com>
2025-01-09 22:27:20 -0300

    Estructura inicial configuración de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
+
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  // Importamos el Router
 import Navbar from "./components/Navbar";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Home from "./pages/Home";
+import Home from "./pages/Home"; //Pagina principal
+// import Register from "./components/Register"; // Importante para hitos futuros
+// import Login from "./components/Login"; // Importante para hitos futuros
+// import Cart frfom "./components/Cart"; // Importante para hitos futuros
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router> {/* Aquí envolvemos toda la app con el Router */}
-      <Navbar />
+      <Navbar /> {/*Barra de navegación */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/register" element={<Register />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
       <Footer />
     </Router>
