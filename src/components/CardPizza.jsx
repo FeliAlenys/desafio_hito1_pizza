ab9f7ca21824f6fb9aa7158fc442158e44c7ecd7
Felipe Gonzalez <felipe.gonzatr@gmail.com>
2025-01-09 23:06:07 -0300

    Actualización de CardPizza: ingredientes dinámicos y nueva prop desc

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -2,14 +2,20 @@
 import { formatPrice } from "../utils/helpers";  // Importamos la función desde helpers.js
 import PropTypes from 'prop-types';
 
-const CardPizza = ({ name, price, ingredients, img }) => (
+const CardPizza = ({ name, price, ingredients, img, desc }) => (
   <div className="card" style={{ width: "18rem", margin: "1rem" }}>
     <img src={img} className="card-img-top" alt={`Imagen de ${name}`} />
     <div className="card-body">
       <h5 className="card-title">{name}</h5>
+      <p className="card-text text-muted">{desc}</p>
       <p className="card-text">
         <strong>Ingredientes:</strong> {ingredients.join(", ")}
       </p>
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
       <p className="card-text">
         <strong>Precio:</strong> {formatPrice(price)} {/* Usamos la función formatPrice aquí */}
       </p>
@@ -24,6 +30,7 @@ CardPizza.propTypes = {
   price: PropTypes.number.isRequired, // Agrega validación para otras props
   ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
   img: PropTypes.string.isRequired,
+  desc: PropTypes.string, // Prop opcional para la descripción
 };
 
 export default CardPizza;
